61857455df5de82a1897c3460d7a223ebbc0c649
Tina Johnson <97616278+tmjohnson112341@users.noreply.github.com>
2022-04-11 12:02:30 -0400

    Update dishes.router.js

--- src/dishes/dishes.router.js
+++ src/dishes/dishes.router.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const controller = require("./dishes.controller");
 const methodNotAllowed = require("../errors/methodNotAllowed");
 
-// TODO: Implement the /dishes routes needed to make the tests pass
+// Implement the /dishes routes
 
 router
   .route("/")
