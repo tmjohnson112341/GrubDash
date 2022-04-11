1cdf4cb500997014059f013247872aa5435229e3
Tina Johnson <97616278+tmjohnson112341@users.noreply.github.com>
2022-04-11 12:03:32 -0400

    Update orders.router.js

--- src/orders/orders.router.js
+++ src/orders/orders.router.js
@@ -1,7 +1,8 @@
 const router = require("express").Router();
 const controller = require("./orders.controller");
 const methodNotAllowed = require("../errors/methodNotAllowed");
-// TODO: Implement the /orders routes needed to make the tests pass
+
+// Implement the /orders routes 
 
 router
   .route("/:orderId")
